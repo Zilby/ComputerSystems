1b288b977703f7d41b5ef3f7d6d406669ad1ca05
Zilby <azilby@gmail.com>
2017-04-09 18:00:23 -0400

    complete rework of central concept

--- hw11/par_malloc.c
+++ hw11/par_malloc.c
@@ -17,39 +17,31 @@
 
 typedef struct nu_free_cell {
   int64_t              size;
-  int64_t              l_id;
   struct nu_free_cell* next;
 } nu_free_cell;
 
 static const int64_t CHUNK_SIZE = 65536;
+static const int64_t CACHE_SIZE = 128;
 static const int64_t CELL_SIZE  = (int64_t)sizeof(nu_free_cell);
+static const int64_t SIZE_LIMIT = 50;
 
 _Atomic int64_t current_id = 0;
 
 __thread int64_t id = 0;
-__thread nu_free_cell* list = 0;
-__thread pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+__thread nu_free_cell* cache = 0;
+__thread int64_t max_size = 0;
 
-//pthread_mutex_t global_mutex;
-static nu_free_cell* nu_free_lists[10000000];
-static pthread_mutex_t mutexes[10000000];
+nu_free_cell* central_list = 0;
+int64_t central_size = 0;
+pthread_mutex_t central_mutex = PTHREAD_MUTEX_INITIALIZER;
 
-void
-init() {
-  if(id == 0) {
-    //pthread_mutex_lock(&(global_mutex));
-    ++current_id;
-    id = current_id;
-    nu_free_lists[id] = list;
-    mutexes[id] = mutex;
-  }
-}
+int counter = 0; // for test purposes
 
 static
 void
-nu_free_list_coalesce(int i)
+nu_free_list_coalesce()
 {
-  nu_free_cell* pp = nu_free_lists[i];
+  nu_free_cell* pp = central_list;
   int free_chunk = 0;
   while (pp != 0 && pp->next != 0) {
     if (((int64_t)pp) + pp->size == ((int64_t) pp->next)) {
@@ -64,14 +56,16 @@ static
 void
 nu_free_list_insert(nu_free_cell* cell)
 {
-  int64_t i = cell->l_id;
-  if (nu_free_lists[i] == 0 || ((uint64_t) nu_free_lists[i]) > ((uint64_t) cell)) {
-    cell->next = nu_free_lists[i];
-    nu_free_lists[i] = cell;
+  pthread_mutex_lock(&(central_mutex));
+    
+  if (central_list == 0 || ((uint64_t) central_list) > ((uint64_t) cell)) {
+    cell->next = central_list;
+    central_list = cell;
+    pthread_mutex_unlock(&(central_mutex));
     return;
   }
 
-  nu_free_cell* pp = nu_free_lists[i];
+  nu_free_cell* pp = central_list;
 
   while (pp->next != 0 && ((uint64_t)pp->next) < ((uint64_t) cell)) {
     pp = pp->next;
@@ -79,26 +73,60 @@ nu_free_list_insert(nu_free_cell* cell)
 
   cell->next = pp->next;
   pp->next = cell;
+  
+  nu_free_list_coalesce();
+  pthread_mutex_unlock(&(central_mutex));
+}
 
-  nu_free_list_coalesce(cell->l_id);
+static
+void
+nu_cache_insert(nu_free_cell* cell)
+{    
+  if (cache == 0 || ((uint64_t) cache) > ((uint64_t) cell)) {
+    cell->next = cache;
+    cache = cell;
+    return;
+  }
+
+  nu_free_cell* pp = cache;
+
+  while (pp->next != 0 && ((uint64_t)pp->next) < ((uint64_t) cell)) {
+    pp = pp->next;
+  }
+
+  cell->next = pp->next;
+  pp->next = cell;
 }
 
 static
 nu_free_cell*
 free_list_get_cell(int64_t size)
 {
-  for(int i = 0; i < current_id; ++i) {
-    pthread_mutex_lock(&(mutexes[i]));
-    nu_free_cell** prev = &(nu_free_lists[i]);
-    for (nu_free_cell* pp = nu_free_lists[i]; pp != 0; pp = pp->next) {
-      if (pp->size >= size) {
-	*prev = pp->next;
-	pthread_mutex_unlock(&(mutexes[i]));
-	return pp;
-      }
-      prev = &(pp->next);
+  pthread_mutex_lock(&(central_mutex));
+  nu_free_cell** prev = &(central_list);
+  for (nu_free_cell* pp = central_list; pp != 0; pp = pp->next) {
+    if (pp->size >= size) {
+      *prev = pp->next;
+      pthread_mutex_unlock(&(central_mutex));
+      return pp;
     }
-    pthread_mutex_unlock(&(mutexes[i]));
+    prev = &(pp->next);
+  }
+  pthread_mutex_unlock(&(central_mutex));
+  return 0;
+}
+
+static
+nu_free_cell*
+cache_get_cell(int64_t size)
+{
+  nu_free_cell** prev = &(cache);
+  for (nu_free_cell* pp = cache; pp != 0; pp = pp->next) {
+    if (pp->size >= size) {
+      *prev = pp->next;
+      return pp;
+    }
+    prev = &(pp->next);
   }
   return 0;
 }
@@ -110,90 +138,102 @@ make_cell()
   void* addr = mmap(0, CHUNK_SIZE, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
   nu_free_cell* cell = (nu_free_cell*) addr; 
   cell->size = CHUNK_SIZE;
-  cell->l_id = id;
   return cell;
 }
 
 void*
 xmalloc(size_t usize)
 {
-  //fprintf(stderr, "TODO: Implement parallel allocator in par_malloc.c\n");
-  init();
   int64_t size = (int64_t) usize;
   
   // space for size
-  int64_t alloc_size = size + (sizeof(int64_t) * 2);
+  int64_t alloc_size = size + sizeof(int64_t);
 
   // space for free cell when returned to list
   if (alloc_size < CELL_SIZE) {
     alloc_size = CELL_SIZE;
   }
 
-  // TODO: Handle large allocations.
-  if (alloc_size > CHUNK_SIZE) {
-    void* addr = mmap(0, alloc_size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
-    *((int64_t*)addr) = alloc_size;
-    *((int64_t*)(addr + sizeof(int64_t))) = id;
-    return addr + (sizeof(int64_t) * 2);
-  }
-
-  nu_free_cell* cell = free_list_get_cell(alloc_size);
-  if (!cell) {
-    cell = make_cell();
-  } 
-
-  // Return unused portion to free list.
-  int64_t rest_size = cell->size - alloc_size;
+  if (alloc_size > CACHE_SIZE) {
   
-  if (rest_size >= CELL_SIZE) {
-    void* addr = (void*) cell;
-    nu_free_cell* rest = (nu_free_cell*) (addr + alloc_size);
-    rest->size = rest_size;
-    rest->l_id = cell->l_id;
-    pthread_mutex_lock(&(mutexes[cell->l_id]));
-    nu_free_list_insert(rest);
-    pthread_mutex_unlock(&(mutexes[cell->l_id]));
-    *((int64_t*)cell) = alloc_size;
+    // TODO: Handle large allocations.
+    if (alloc_size > CHUNK_SIZE) {
+      void* addr = mmap(0, alloc_size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+      *((int64_t*)addr) = alloc_size;
+      return addr + sizeof(int64_t);
+    }
+
+    nu_free_cell* cell = free_list_get_cell(alloc_size);
+    if (!cell) {
+      cell = make_cell();
+    } 
+
+    // Return unused portion to free list.
+    int64_t rest_size = cell->size - alloc_size;
+  
+    if (rest_size >= CELL_SIZE) {
+      void* addr = (void*) cell;
+      nu_free_cell* rest = (nu_free_cell*) (addr + alloc_size);
+      rest->size = rest_size;
+      nu_free_list_insert(rest);
+      *((int64_t*)cell) = alloc_size;
+    }
+    return ((void*)cell) + sizeof(int64_t);
+  } else {
+    nu_free_cell* cell = cache_get_cell(alloc_size);
+    if (!cell) {
+      cell = free_list_get_cell(alloc_size);
+      if (!cell) {
+	cell = make_cell();
+      }
+    } else {
+      max_size--;
+    }
+    
+    int64_t rest_size = cell->size - alloc_size;
+  
+    if (rest_size >= CELL_SIZE) {
+      void* addr = (void*) cell;
+      nu_free_cell* rest = (nu_free_cell*) (addr + alloc_size);
+      rest->size = rest_size;
+      nu_cache_insert(rest);
+      *((int64_t*)cell) = alloc_size;
+    }
+    return ((void*)cell) + sizeof(int64_t);
   }
-  return ((void*)cell) + (sizeof(int64_t) * 2);
 }
 
 void
 xfree(void* addr)
 {
   //fprintf(stderr, "TODO: Implement parallel allocator in par_malloc.c\n");
-  init();
-  nu_free_cell* cell = (nu_free_cell*)(addr - (sizeof(int64_t) * 2));
+  nu_free_cell* cell = (nu_free_cell*)(addr - sizeof(int64_t));
   int64_t size = cell->size;
-  int64_t cell_id = cell->l_id;
-
-  pthread_mutex_lock(&(mutexes[cell_id]));
-
-  if (size > CHUNK_SIZE) {
-    munmap((void*) cell, size);
-  }
-  else {
+  if(cell->size > CACHE_SIZE || max_size > SIZE_LIMIT) {    
+    if (size > CHUNK_SIZE) {
+      munmap((void*) cell, size);
+    }
+    else {
+      cell->size = size;
+      nu_free_list_insert(cell);
+    }
+  } else {
     cell->size = size;
-    cell->l_id = cell_id;
-    nu_free_list_insert(cell);
+    nu_cache_insert(cell);
+    max_size++;
   }
-
-  pthread_mutex_unlock(&(mutexes[cell_id]));
 }
 
 void*
 xrealloc(void* ptr, size_t size)
 {
   //fprintf(stderr, "TODO: Implement parallel allocator in par_malloc.c\n");
-  init();
   void* new_ptr;
   if(size > 0) {
     new_ptr = xmalloc(size);
-    pthread_mutex_lock(&(mutexes[id]));
-    nu_free_cell* cell = (nu_free_cell*)(ptr - (sizeof(int64_t) * 2));
+    nu_free_cell* cell = (nu_free_cell*)(ptr - sizeof(int64_t));
     int64_t size_old = *((int64_t*) cell);
     memcpy(new_ptr, ptr, size_old);
-    pthread_mutex_unlock(&(mutexes[id]));
   }
   xfree(ptr);
   return new_ptr;
